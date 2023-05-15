de9702487527cf1d11e50518af893f8936a63680
Victor Raul Medina Meza <vrmm1998@gmail.com>
2023-05-14 23:41:24 -0500

    add entity framework and dbcontext

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace d_Videogame_Store.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Client> Clients => Set<Client>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using d_Videogame_Store.Data;
 global using d_Videogame_Store.Models;
 // Client
 global using d_Videogame_Store.DTOs.Client;
@@ -11,6 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => 
